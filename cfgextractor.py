60a5cb227ad1a722119eb7e2da1b1d4bd076a8ee
Devereux Henley <devereux@ksu-wireless-15-210.campus.ksu.edu>
2016-07-18 13:18:35 -0400

    setting up command line input

--- cfgextractor.py
+++ cfgextractor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from openpyxl import Workbook
+import sys
 
 def output(xaxis, yaxis, data):
     wb = Workbook()
@@ -23,5 +24,16 @@ def output(xaxis, yaxis, data):
 
 
     wb.save('output.xlsx')
+
+def start(xfield,xfiles,yfield,yfiles,pivot,data):
+
+failstring = ("requires 6 arguments: xaxis field, list of files containing xaxis field,
+             yaxis field, list of files containing yaxis field, pivot field, and desir
+             ed data fields.")
+
+if len(sys.argv) != 7:
+    print(sys.argv[0] + failstring)
+else:
+    start(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
 
 output(['a','b','c'],['1','2','3'],[['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3']])    
