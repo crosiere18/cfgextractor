bee5c4e352f321b5c6e6d492ffdaed67107972a0
Devereux Henley <devereux@ksu-wireless-15-210.campus.ksu.edu>
2016-07-18 14:53:36 -0400

    dechunkify finished. Now just need data matching

--- cfgextractor.py
+++ cfgextractor.py
@@ -25,16 +25,70 @@ def output(xaxis, yaxis, data):
 
     wb.save('output.xlsx')
 
-def start(xfield,xfiles,yfield,yfiles,pivot,data):
+def readin(files):
+    ret = {}
+    chunk = {}
+    chunknum = 0 
+    for fname in files:
+        cfgfile = open(fname, 'r')
+        
+        for line in cfgfile:
+            line = line.strip()
+            
+            if not line or line.startswith('#') or line.startswith('define'): continue
+            
+            if line.startswith('}'):
+                ret[chunknum] = chunk
+                chunknum += 1
+                chunk = {}
+                continue 
+
+            bits = line.split(None, 1)
+
+            if len(bits) > 1:
+                k, v = bits
+                chunk[k] = v
+
+        cfgfile.close()
+    return ret
+
+def dechunkify(delim, axis, chunkmap, field):
+    for chunk in chunkmap:
+        if field in chunkmap[chunk]:
+            values = chunkmap[chunk][field].split(delim)
+            for item in values:
+                if item and item not in axis:
+                    axis.append(item)
+
+def start(filescfg,xfield,yfield,pivot,data):
+    xaxis = []
+    yaxis = []
+
+    delim = ','
+    
+    infiles = open(filescfg, 'r')
+    xfiles = infiles.readline().strip().split(delim)
+    yfiles = infiles.readline().strip().split(delim)
+
+    xchunkmap = readin(xfiles)
+    ychunkmap = readin(yfiles) 
+
+    dechunkify(delim, xaxis, xchunkmap, xfield)
+    dechunkify(delim, yaxis, ychunkmap, yfield)
+
+    print(xaxis)
+    print(yaxis)
+
     return "blah"
 
-failstring = ("requires 6 arguments: xaxis field, list of files containing xaxis field,"
-             "yaxis field, list of files containing yaxis field, pivot field, and desir"
-             "ed data fields.")
+if __name__ == "__main__":
+    failstring = ("requires 5 arguments: xaxis field, list of files containing xaxis field,"
+                "yaxis field, list of files containing yaxis field, pivot field, and desir"
+                "ed data fields.")
 
-if len(sys.argv) != 7:
-    print(sys.argv[0] + failstring)
-else:
-    start(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    if len(sys.argv) != 6:
+        print(sys.argv[0] + failstring)
+    else:
+        start(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
 
 output(['a','b','c'],['1','2','3'],[['a1','a2','a3'],['b1','b2','b3'],['c1','c2','c3']])    
